db31fb57f0bb98fdc286d938f13ecd26affe9511
unkown <153752956+lad99071@users.noreply.github.com>
2023-12-13 22:27:27 +0530

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -1,5 +1,3 @@
-
-/* KSHITIJ RAMAKANT BHALSANE SE A 10
 PROBLEM STATEMENT - 
 Write a C++ program that creates an output file, writes information to it, closes the file, open
 it again as an input file and read the information from the file.
