2e658d9d91a89871b557e264cd61280c044f3057
unkown <153752956+lad99071@users.noreply.github.com>
2023-12-13 22:05:55 +0530

    Update operator.cpp

--- operator.cpp
+++ operator.cpp
@@ -1,74 +1,62 @@
 #include<iostream>
-
 using namespace std;
-
 class Complex
 {
-	int real, imag ;
-
 	public :
-
-		Complex(int r = 0 , int i = 0)
-		{
-			real = r;
-			imag = i;
-		}
-
-
-		Complex operator+(Complex c)
-		{
-			Complex temp;
-
-			temp.real = real+c.real;
-			temp.imag = imag+c.imag;
-			return temp;
-		}
-
-		Complex operator*(Complex c)
-		{
-			Complex temp;
-
-			temp.real = real*c.real - imag*c.imag;
-			temp.imag = real*c.imag + imag*c.real;
-			return temp;
-		}
-
-		friend istream& operator>>(istream& , Complex& );
-		friend ostream& operator<<(ostream& , Complex& );
-
+	float real,img;
+	Complex()
+	{}
+	Complex operator +(Complex);
+	Complex operator *(Complex);
+	friend ostream & operator <<(ostream&,Complex&);
+	friend istream & operator >>(istream&,Complex&);
 };
 
-		istream& operator>>(istream& cin , Complex& c)
-		{
-			cin>> c.real;
-			cin>>c.imag;
-			return cin;
-		}
+	Complex Complex::operator +(Complex obj)
+	{
+	Complex temp;
+	temp.real=real+obj.real;
+	temp.img=img+obj.img;
+	return temp;
+	}
 
-		ostream& operator<<(ostream& cout , Complex& c)
-		{
-			cout<<c.real <<" + ";
-			cout<<c.imag<<"i";
-			return cout;
-		}
+	Complex Complex::operator *(Complex obj)
+	{
+	Complex temp;
+	temp.real=real*obj.real-img*obj.img;
+	temp.img=real*obj.img+img*obj.real;
+	return temp;
+	}
 
+//definition of istream
+	istream& operator >>(istream& is,Complex& obj)
+	{
+	is>>obj.real;
+	is>>obj.img;
+	return is;
+	}
+//definition of ostream
+	ostream& operator <<(ostream& os,Complex& obj)
+	{
+	os<<obj.real;
+	os<<"+"<<obj.img<<"i";
+	return os;
+	}
 int main()
 {
-	Complex c1,c2,c3,c4;
-	int r1,r2 , i1 , i2;
+	Complex a,b,c,d,e;
+	cout<<endl<<"enter 1st no"<<endl;
+	cout<<"enter real and img. no = "<<endl;
+	cin>>a;
+	cout<<endl<<"enter 2nd no"<<endl;
+	cout<<"enter real and img. no = "<<endl;
+	cin>>b;
+	cout<<endl<<"arithmatic operations are"<<endl;
+	c=a+b;
+	cout<<"addition is= "<<c<<endl;
+	d=a*b;
+	cout<<"multiplication is ="<<d<<endl<<endl;
 
-	cout<<"Enter the Complex number : ";
-	cin>>c1;
-
-	cout<<"Enter the Complex number : ";
-	cin>>c2;
-
-
-	c3 = c1 + c2;
-	c4 = c1*c2;	
-	cout<<"Their Sum is : "<<c3 << endl;
-
-	cout<<"Their Product is : "<<c4 << endl;
 
 	return 0;
 }
